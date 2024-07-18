ae338e42e26718b35c7fda1c22b0298df27c094c
Jesmond Giordmaina <jgiordmaina@hsdyn.com>
2024-07-18 21:12:59 +0100

    chore: Update welcome message in Azure Functions endpoint

--- JGSpaceAzureFunction1.cs
+++ JGSpaceAzureFunction1.cs
@@ -18,7 +18,7 @@ namespace Company.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult("Welcome to Azure Functions! - JG");
         }
     }
 }
