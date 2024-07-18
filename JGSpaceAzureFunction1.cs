5552c6c033cbe21e4252c780c1ef977c7cc45d06
Jesmond Giordmaina <jgiordmaina@hsdyn.com>
2024-07-18 19:59:23 +0100

    chore: Add VS Code configuration files for Azure Functions project

---
+++ JGSpaceAzureFunction1.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class JGSpaceAzureFunction1
+    {
+        private readonly ILogger<JGSpaceAzureFunction1> _logger;
+
+        public JGSpaceAzureFunction1(ILogger<JGSpaceAzureFunction1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("JGSpaceAzureFunction1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
