0119dbe895b677537ba3bc307c3fbc7cf101558a
Jesmond Giordmaina <jgiordmaina@hsdyn.com>
2024-07-18 22:22:19 +0100

    RPS

--- JGSpaceAzureFunction1.cs
+++ JGSpaceAzureFunction1.cs
@@ -15,10 +15,34 @@ namespace Company.Function
         }
 
         [Function("JGSpaceAzureFunction1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req, string rpsInput)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions! - JG");
+            string responseMessage = string.IsNullOrEmpty(rpsInput)
+                ? "No input detected - Please add {\"rpsInput\": \"rock\" }"
+                : $"You chose \"{rpsInput}\".";
+
+                string[] validInputs = { "rock", "paper", "scissors" };
+                string computerChoice = validInputs[new Random().Next(validInputs.Length)];
+
+                string result;
+                if (rpsInput == computerChoice)
+                {
+                    result = "It's a tie!";
+                }
+                else if ((rpsInput == "rock" && computerChoice == "scissors") ||
+                         (rpsInput == "paper" && computerChoice == "rock") ||
+                         (rpsInput == "scissors" && computerChoice == "paper"))
+                {
+                    result = "You win!";
+                }
+                else
+                {
+                    result = "You lose!";
+                }
+
+                responseMessage += $" The computer chose {computerChoice}. Result: {result}";
+            return new OkObjectResult(responseMessage);
         }
     }
 }
